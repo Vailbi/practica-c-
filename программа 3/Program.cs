2d55020b7a75bc8e1f639d8ed2a7db82947c4f28
Fedor <vailbi94@gmail.com>
2022-06-08 22:51:48 +0300

    random

--- программа 3/Program.cs
+++ программа 3/Program.cs
@@ -1,4 +1,5 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10);
+int numberB = new Random().Next(1,10);
 int result = numberA + numberB;
+Console.WriteLine (numberA, numberB);
 Console.WriteLine(result);
